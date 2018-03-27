31c20029b5928b022e7212e5ef7ac2c312de7237
Belinda Li <lib49@cs.washington.edu>
2018-03-27 00:26:44 -0700

    Batching+substantial changes to data processing

--- rt-polarity/parser.py
+++ rt-polarity/parser.py
@@ -1,17 +1,52 @@
+from torchtext import data
+import torch
+import random
+import spacy
+import numpy as np
+
+spacy_en = spacy.load('en')
+
 # 0 is used to denote "unknown"/"empty"
-type_priorpolarity_to_polarity = {('strongsubj', 'negative'):1,
-                                  ('weaksubj', 'negative'):2,
-                                  ('weaksubj', 'neutral'):3,
-                                  ('strongsubj', 'neutral'):3,
-                                  ('weaksubj', 'positive'):4,
-                                  ('strongsubj', 'positive'):5 }
+type_priorpolarity_to_polarity = {('strongsubj', 'negative'):'1',
+                                  ('weaksubj', 'negative'):'2',
+                                  ('weaksubj', 'neutral'):'3',
+                                  ('strongsubj', 'neutral'):'3',
+                                  ('weaksubj', 'positive'):'4',
+                                  ('strongsubj', 'positive'):'5' }
 
 def parse_tff(filename, word_to_ix):
     # has identical words to word_to_ix
     word_to_polarity = {}
+    if word_to_ix is not None:
+        # initialize all words from word_to_ix to "unknown"
+        for word in word_to_ix:
+            word_to_polarity[word] = 0
+    # read words which already have an associated polarity
+    print("opening " + filename + "...")
+    with open("../resources/" + filename, 'rt') as f:
+        i = 0
+        for line in f:
+            word_line = line.split()
+            word = (word_line[2].split("="))[1]
+            if word_to_ix is not None:
+                # don't include words not in word_to_ix
+                if word not in word_to_ix:
+                    continue
+            type_ = (word_line[0].split("="))[1]
+            priorpolarity = (word_line[5].split("="))[1]
+            if priorpolarity == "both":
+                continue
+            polarity = type_priorpolarity_to_polarity[(type_, priorpolarity)]
+            word_to_polarity[word] = polarity
+            #print(word_to_polarity[word])
+    return word_to_polarity
+
+def parse_tff_ixs(filename, word_to_ix):
+    # has identical words to word_to_ix
+    ix_to_polarity = {}
     # initialize all words from word_to_ix to "unknown"
     for word in word_to_ix:
-        word_to_polarity[word] = 0
+        ix_to_polarity[word_to_ix[word]] = 0
     # read words which already have an associated polarity
     print("opening " + filename + "...")
     with open("../resources/" + filename, 'rt') as f:
@@ -21,14 +56,15 @@ def parse_tff(filename, word_to_ix):
             word = (word_line[2].split("="))[1]
             # don't include words not in word_to_ix
             if word not in word_to_ix:
-                continue
+                    continue
             type_ = (word_line[0].split("="))[1]
             priorpolarity = (word_line[5].split("="))[1]
             if priorpolarity == "both":
                 continue
             polarity = type_priorpolarity_to_polarity[(type_, priorpolarity)]
-            word_to_polarity[word] = polarity
-    return word_to_polarity
+            ix_to_polarity[word_to_ix[word]] = polarity
+            #print(word_to_polarity[word])
+    return ix_to_polarity
 
 def parse_embeddings(filename):
     word_to_ix = {} # index of word in embeddings
@@ -50,9 +86,106 @@ def parse_embeddings(filename):
             """
     return word_to_ix, embeds
 
-def splitdata(X):
+class MR(data.Dataset):
+    @staticmethod
+    def sort_key(ex):
+        return len(ex.text)
+
+    def __init__(self, text_field, polarity_field, label_field, path, pre_examples):
+        """ Create an MR dataset instance given a path and fields.
+        Arguments:
+            text_field: The field that will be used for text data.
+            label_field: The field that will be used for label data.
+            path: Path to the data file.
+            examples: The examples contain all the data.
+            Remaining keyword arguments: Passed to the constructor of
+                data.Dataset.
+        """
+        #self.fields = {'text':[('text',text_field)], 'label':[('label',label_field)]}
+        #self.fields = [('text', text_field), ('polarity', polarity_field), ('label', label_field)]
+        self.fields = [('text', text_field), ('polarity', polarity_field), ('label', label_field)]
+
+        #path = self.dirname if path is None else path
+        #print(self.fields.items())
+        self.examples = []
+        '''
+        with open(str(path) + 'rt-polarity.neg','r', encoding='latin1') as f:
+            # append to examples for each line
+            self.examples += [
+                data.Example.fromlist([line, 0], fields) for line in f]
+        with open(str(path) + 'rt-polarity.pos','r', encoding='latin1') as f:
+            # append to examples for each line
+            self.examples += [
+                data.Example.fromlist([line, 1], fields) for line in f]
+        '''
+        for pre_ex in pre_examples:
+            example = data.Example.fromlist(pre_ex, self.fields)
+            self.examples.append(example)
+            #print(str(example.text) + " " + str(example.polarity))
+        print(len(self.examples))
+        super(MR, self).__init__(self.examples, self.fields)
+    
+    def get_examples():
+        return self.examples
+
+# does data preprocessing whereas other doesn't
+def splitdata_batches(X, batch_size, embedding_dim):
+    train, dev, test = splitdata(X, batch_size)
+    print('setting up data')
+    #'''
+    TEXT = data.Field(sequential=True, tokenize=tokenizer)#, lower=True)
+    # lower means to lowercase
+    POLARITY = data.Field(sequential=True, tokenize=get_polarity)
+    LABEL = data.Field(sequential=False, use_vocab=False)
+
+    # load data
+    print('loading data')
+    Xtrain = MR(TEXT, POLARITY, LABEL, None, train)
+    Xdev = MR(TEXT, POLARITY, LABEL, None, dev)
+    Xtest = MR(TEXT, POLARITY, LABEL, None, test)
+    #print(Xtrain.fields.items())
+    
+    # load embeddings
+    TEXT.build_vocab(Xtrain)
+    POLARITY.build_vocab(Xtrain) # should just be 0-5
+
+    print(POLARITY.vocab.stoi)
+
+    print('loading glove embeddings')
+    TEXT.vocab.load_vectors('glove.6B.' + str(embedding_dim) + 'd')
+
+    print('building batches')
+    Xtrain, Xdev, Xtest = data.Iterator.splits(
+        (Xtrain, Xdev, Xtest),
+        batch_sizes=(batch_size, len(Xdev), len(Xtest)),
+        repeat=False,
+        device = -1 # run on CPU
+    )
+    '''
+    Xtrain = []
+    Xdev = dev
+    Xtest = test
+    for i in range(0, len(train), batch_size):
+        words = []
+        labels = []
+        for j in range(i,min(i+batch_size, len(train))):
+            words.append(train[j][0])
+            labels.append(train[j][1])
+        Xtrain.append([words, labels])
+    '''
+    return Xtrain, Xdev, Xtest, TEXT
+
+def splitdata(X, batch_size):
+    print('splitting data')
+    # shuffle data
+    X = random.sample(X, len(X))
+    
     trainlen = int(0.8 * len(X))
-    devlen = int(0.1 * len(X))
+    # ensure all batches are the same size
+    while trainlen % batch_size != 0:
+        trainlen -= 1
+    
+    devlen = int(0.5 * (len(X) - trainlen))
     Xtrain = []
     Xdev = []
     Xtest = []
@@ -66,15 +199,56 @@ def splitdata(X):
     print(len(Xtrain))
     print(len(Xdev))
     print(len(Xtest))
-
     return Xtrain, Xdev, Xtest
 
-def parse_input_files(filename, label):
+def tokenizer(text):
+    to_ret = [tok.text for tok in spacy_en.tokenizer(text)]
+    #print(to_ret)
+    return to_ret
+
+word_to_polarity = parse_tff("subjclueslen1-HLTEMNLP05.tff", None)
+
+def get_polarity(text):
+    tokens = tokenizer(text)
+    to_ret = []
+    for text in tokens:
+        if text in word_to_polarity:
+            to_ret.append(word_to_polarity[text])
+        else: # unknown
+            to_ret.append('<unk>')
+        if not (isinstance(to_ret[len(to_ret)-1], str)):
+            print(to_ret[len(to_ret)-1])
+    return to_ret
+
+def parse_input_files(filepath, batch_size):
     """
     Reads the file with name filename
     """
+    '''
+    TEXT = data.Field(sequential=True, tokenize=tokenizer)#, lower=True)
+    # lower means to lowercase
+    LABEL = data.Field(sequential=False, use_vocab=False)
+
+    Xs = MR(TEXT, LABEL, 'rt-polaritydata/')
+
+    splitdata_batches(Xs, batch_size)
+    
+    # load embeddings
+    TEXT.build_vocab(train, vectors="glove.6B." + str(embedding_dim) + "d")
+    '''
     xs = []
-    with open(filename, 'rt', encoding='latin1') as f:
+    # positive = 1
+    with open(filepath + "rt-polarity.pos", 'rt', encoding='latin1') as f:
         for line in f:
-            xs.append((line.split(), label))
+            if batch_size > 1: # will be in batches
+                xs.append([line, line, 1])
+            else:
+                xs.append([line.split(), 1])
+    # negative = 0
+    with open(filepath + "rt-polarity.neg", 'rt', encoding='latin1') as f:
+        for line in f:
+            if batch_size == 1:
+                xs.append([line.split(), 0])
+            else: # will be in batches
+                xs.append([line, line, 0])
     return xs
---
+++ rt-polarity/rt-polarity-embeds-lstm-attention-batch.py
@@ -0,0 +1,258 @@
+import os
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import autograd
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import random
+from random import shuffle
+import matplotlib.pyplot as plt
+import parser
+
+epochs = 10
+EMBEDDING_DIM = 50
+HIDDEN_DIM = 2 * EMBEDDING_DIM
+NUM_FEATURES = 6
+BATCH_SIZE = 5
+
+class Model(nn.Module):
+    def __init__(self, num_labels, vocab_size, embeddings_size,
+                 hidden_dim, word_embeddings, num_features, batch_size):
+        super(Model, self).__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+
+        self.word_embeds = nn.Embedding(vocab_size, embeddings_size)
+        self.word_embeds.weight.data.copy_(torch.FloatTensor(word_embeddings))
+        self.word_embeds.weight.requires_grad = False # don't update the embeddings
+        
+        self.feature_embeds = nn.Embedding(num_features+1, embeddings_size) # add 1 for <pad>
+        
+        # The LSTM takes [word embeddings, feature embeddings] as inputs, and
+        # outputs hidden states with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(2 * embeddings_size, hidden_dim)
+
+        # The linear layer that maps from hidden state space to target space
+        self.hidden2label = nn.Linear(hidden_dim, num_labels)
+        self.hidden = self.init_hidden()
+
+        # Matrix of weights for each layer
+        # Linear map from hidden layers to alpha for that layer
+        self.attention = nn.Linear(hidden_dim, 1)
+
+    def init_hidden(self):
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        # Initialize to 0's
+        # Returns (hidden state, cell state)
+        return (autograd.Variable(torch.zeros(1, self.batch_size, self.hidden_dim)),
+                autograd.Variable(torch.zeros(1, self.batch_size, self.hidden_dim)))
+
+    def forward(self, word_vec, feature_vec): # how to classify a training example?
+        # Apply embeddings & prepare input
+        word_embeds_vec = self.word_embeds(word_vec).view(len(word_vec), self.batch_size, -1)
+        feature_embeds_vec = self.feature_embeds(feature_vec).view(len(feature_vec), self.batch_size, -1)
+        # [word embeddings, feature embeddings]
+        lstm_input = torch.cat((word_embeds_vec, feature_embeds_vec),
+                               1).view(len(word_vec), self.batch_size, -1)
+
+        # Pass through lstm
+        lstm_out, self.hidden = self.lstm(lstm_input, self.hidden)
+
+        '''
+        # Compute and apply weights (attention) to each layer
+        alphas = self.attention(lstm_out)
+        alphas = F.softmax(alphas, dim=0)
+        print(alphas)
+        weighted_lstm_out = torch.sum(torch.mul(alphas, lstm_out), dim=0)
+        print(lstm_out)
+        print(weighted_lstm_out)
+        print
+        '''
+        weighted_lstm_out = lstm_out[-1]
+        
+        # Get final results, passing in weighted lstm output:
+        tag_space = self.hidden2label(weighted_lstm_out)
+        #print("tags = " + str(tag_space))
+        log_probs = F.log_softmax(tag_space, dim=1)
+        return log_probs
+
+# includes word and polarity ("feature") embeddings
+def make_embeddings_vector(sentence, word_to_ix, word_to_polarity):
+    print(sentence)
+    word_vec = []
+    feature_vec = []
+    for word in sentence:
+        # domains of word_to_ix and word_to_polarity are equal,
+        # so just need to check word_to_ix
+        if word in word_to_ix:
+            word_vec.append(word_to_ix[word])
+            feature_vec.append(word_to_polarity[word])
+            #print(word + " " + str(word_to_polarity[word]))
+    print(word_vec)
+    return autograd.Variable(torch.LongTensor(word_vec)), autograd.Variable(
+        torch.LongTensor(feature_vec))
+
+def train(Xtrain, model, word_to_ix,# ix_to_polarity,
+          Xdev, Xtest):
+    '''
+    # shuffle training samples--already shuffled when we split the data
+    Xtrain = random.sample(Xtrain, len(Xtrain))
+    '''
+    print("Evaluating before training...")
+    trains = []
+    devs = []
+    tests = []
+    trainposperf, trainnegperf, trainperf = evaluate(model, word_to_ix,# ix_to_polarity,
+                                                     Xtrain)
+    print("train correctly negative: " + str(trainnegperf))
+    print("train correctly positive: " + str(trainposperf))
+    print("train correctly correct: " + str(trainperf))
+    devposperf, devnegperf, devperf = evaluate(model, word_to_ix,# ix_to_polarity,
+                                               Xdev)
+    print("dev correctly negative: " + str(devnegperf))
+    print("dev correctly positive: " + str(devposperf))
+    print("dev correctly correct: " + str(devperf))
+    trains.append(trainperf)
+    devs.append(devperf)
+    tests.append(evaluate(model, word_to_ix,# ix_to_polarity,
+                          Xtest)[2])
+    
+    loss_function = nn.NLLLoss()
+
+    # skip updating the non-requires-grad params (i.e. the embeddings)
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
+    
+    for epoch in range(0,epochs):
+        print("Epoch " + str(epoch))
+        i = 0
+        for batch in Xtrain:
+            words, polarity, label = batch.text, batch.polarity, batch.label
+            
+            # Step 1. Remember that Pytorch accumulates gradients.
+            # We need to clear them out before each instance
+            model.zero_grad()
+            model.batch_size = len(label.data) # set batch size
+            
+            model.hidden = model.init_hidden() # detach from last instance
+            '''
+            # Step 2. Make our BOW vector and also we must wrap the target in a
+            # Variable as an integer. For example, if the target is NEGATIVE, then
+            # we wrap the integer 0. The loss function then knows that the 0th
+            # element of the log probabilities is the log probability
+            # corresponding to NEGATIVE
+            words_vec, features_vec = make_embeddings_vector(instance, word_to_ix, word_to_polarity)
+            target = autograd.Variable(torch.LongTensor([label])) #make_target(1, label_to_ix))
+            '''
+            # Step 3. Run our forward pass.
+            log_probs = model(words, polarity)
+            
+            # Step 4. Compute the loss, gradients, and update the parameters by
+            # calling optimizer.step()
+            loss = loss_function(log_probs, label) # log_probs = actual distr, target = computed distr
+            loss.backward()
+            optimizer.step()
+            #print("loss = " + str(loss))
+            if (i % 100 == 0):
+                print("    " + str(i))
+            i += 1
+        print("Evaluating...")
+        trainposperf, trainnegperf, trainperf = evaluate(model, word_to_ix, Xtrain)
+        print("train correctly negative: " + str(trainnegperf))
+        print("train correctly positive: " + str(trainposperf))
+        print("train correctly correct: " + str(trainperf))
+        devposperf, devnegperf, devperf = evaluate(model, word_to_ix, Xdev)
+        print("dev correctly negative: " + str(devnegperf))
+        print("dev correctly positive: " + str(devposperf))
+        print("dev correctly correct: " + str(devperf))
+        trains.append(trainperf)
+        devs.append(devperf)
+        tests.append(evaluate(model, word_to_ix, Xtest)[2])
+    return tests, devs, trains
+
+def evaluate(model, word_to_ix,# ix_to_polarity,
+             Xs):
+    poscount = 0
+    negcount = 0
+    totalpos = 0
+    totalneg = 0
+    # count positive classifications in pos
+    for batch in Xs:
+        #print(word_to_ix)
+        words, label = batch.text, batch.label
+        #print(words)
+        polarity = batch.polarity
+        model.batch_size = len(label.data) # set batch size
+        model.hidden = model.init_hidden()  # detaching it from its history on the last instance.
+        #f_vector = make_polarity_vector(words, ix_to_polarity)
+        log_probs = model(words, polarity)
+        #print(str(label.data) + " " + str(log_probs))
+        for instance_label in label.data:
+            if instance_label == 1: # real label is positive
+                totalpos += 1
+                if (log_probs.data[instance_label][0] < log_probs.data[instance_label][1]): # classify (correctly) as positive
+                    poscount += 1
+            else: # real label is negative
+                totalneg += 1
+                if (log_probs.data[instance_label][0] > log_probs.data[instance_label][1]): # classify (correctly) as negative
+                    negcount += 1
+        #print(str(totalpos) + " " + str(poscount) + " " + str(totalneg) + " " + str(negcount))
+    print("neg " + str(totalneg))
+    print("pos " + str(totalpos))
+    pos = float(poscount) / float(totalpos)
+    neg = float(negcount) / float(totalneg)
+    total = float(poscount + negcount) / float(totalpos + totalneg)
+    return pos, neg, total
+
+# plot across each epoch
+def plot(dev_accs, train_accs):
+    plt.plot(range(0,epochs+1), dev_accs, c = 'red', label = 'Dev Set Accuracy')
+    plt.plot(range(0,epochs+1), train_accs, c = 'blue', label = 'Train Set Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy vs. # of Epochs')
+    plt.legend()
+    plt.show()
+
+def main():
+    Xs = parser.parse_input_files("./rt-polaritydata/", BATCH_SIZE)
+    #print(Xneg)
+    #Xpos = parser.parse_input_files("rt-polaritydata/rt-polarity.pos", 1, EMBEDDING_DIM)
+    
+    Xtrain, Xdev, Xtest, text_field = parser.splitdata_batches(Xs, BATCH_SIZE, EMBEDDING_DIM)
+    #Xpostrain, Xposdev, Xpostest = parser.splitdata(Xpos)#, BATCH_SIZE)
+
+    word_to_ix = text_field.vocab.stoi
+    #ix_to_polarity = polarity_field.vocab.itos
+    
+    #word_to_ix, embeds = parser.parse_embeddings("glove.6B." + str(EMBEDDING_DIM) + "d.txt")
+    # returns mapping word->polarity for shared words in word->ix
+    #ix_to_polarity = parser.parse_tff_ixs("subjclueslen1-HLTEMNLP05.tff", word_to_ix)
+
+    print(text_field.vocab.vectors)
+    
+    NUM_LABELS = 2
+    VOCAB_SIZE = len(word_to_ix)
+
+    word_embeds = text_field.vocab.vectors
+
+    model = Model(NUM_LABELS, VOCAB_SIZE,
+                  EMBEDDING_DIM, HIDDEN_DIM, word_embeds,
+                  NUM_FEATURES, BATCH_SIZE)
+    
+    test_c, dev_c, train_c = train(Xtrain, model,
+                                   word_to_ix,# ix_to_polarity,
+                                   Xdev, Xtest)
+    '''
+    plot(dev_c, train_c)
+
+    print(str(dev_c))
+    best_epochs = np.argmax(np.array(dev_c))
+    dev_results = dev_c[best_epochs]
+    print("Test performance = " + str(test_c[best_epochs]))
+    '''
+
+if __name__ == "__main__":
+    main()
